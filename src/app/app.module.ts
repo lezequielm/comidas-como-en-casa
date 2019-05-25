5347918d67efdb3b49df6a779378fd4e5cba0564
emartin <emartin@aconcaguasf.com>
2019-05-24 21:26:33 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {MealsComponent} from './meals/meals.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatCheckboxModule, MatListModule, MatTableModule, MatToolbarModule} from '@angular/material';
+import {MealService} from './services/meal.service';
+import { AddComponent } from './meals/add/add.component';
+
+const appRoutes: Routes = [
+  {path: 'meals', component: MealsComponent},
+  {path: 'meals/add', component: AddComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MealsComponent,
+    AddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatTableModule,
+    MatCheckboxModule,
   ],
-  providers: [],
+  providers: [
+    MealService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
